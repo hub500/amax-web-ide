ef3c65f5754ee91d75c3a2747ef870b76a07f243
hub500 <80018598+hub500@users.noreply.github.com>
2023-03-26 00:50:14 +0800

    Create deploy-amax.token.sh

---
+++ deploy-amax.token.sh
@@ -0,0 +1,13 @@
+
+amcli create account amax amax.token AM6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV -p amax@active
+
+cd /home/gitpod/contracts
+
+amcli set code amax.token amax.token.wasm -p amax.token@active
+amcli set abi amax.token amax.token.abi -p amax.token@active
+
+amcli push action amax.token create '[ "amax", "1000000000.00000000 AMAX"]' -p amax.token@active
+amcli push action amax.token issue '[ "amax", "1000000000.00000000 AMAX", "amax issue"]' -p amax@active
+
+amcli create account amax myusermyuser AM6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV -p amax@active
+amcli push action amax.token transfer '["amax","myusermyuser","100.00000000 AMAX",""]' -p amax@active
